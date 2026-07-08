7bf731223dcfd42ecaf7577c6462eb00a45616a5
Lee Moody <lee.moody@esynergy.co.uk>
2026-07-08 07:38:34 +0100

    Add repository and outer layer paging extensions

---
+++ src/Strategies.Abstractions/Repositories/Pagination/QueryablePagingExtensions.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="QueryablePagingExtensions.cs" company="Defra">
+// Copyright (c) Defra. All rights reserved.
+// </copyright>
+
+namespace Defra.Livestock.Sdk.Api.Strategies.Abstractions.Repositories.Pagination;
+
+using System.Linq.Expressions;
+
+public static class QueryablePagingExtensions
+{
+    public static async Task<PagedEntities<TEntity>> ToPaged<TEntity, TOrderBy>(
+        this IQueryable<TEntity> query,
+        int pageNumber,
+        int pageSize,
+        Expression<Func<TEntity, TOrderBy>> orderBy,
+        bool orderByDescending,
+        Func<IQueryable<TEntity>, CancellationToken, Task<int>> countAsyncFunc,
+        Func<IQueryable<TEntity>, CancellationToken, Task<List<TEntity>>> toListAsyncFunc,
+        CancellationToken cancellationToken = default)
+    {
+        query = orderByDescending ? query.OrderByDescending(orderBy) : query.OrderBy(orderBy);
+
+        var totalCount = await countAsyncFunc(query, cancellationToken);
+        var totalPages = (totalCount + pageSize - 1) / pageSize;
+
+        var pagedQuery = query
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize);
+
+        var resultsList = await toListAsyncFunc(pagedQuery, cancellationToken);
+
+        return new PagedEntities<TEntity>(resultsList, totalCount, totalPages, pageNumber, pageSize);
+    }
+}
---
+++ src/Strategies.Abstractions/Responses/PagedEntitiesExtensions.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="PagedEntitiesExtensions.cs" company="Defra">
+// Copyright (c) Defra. All rights reserved.
+// </copyright>
+
+namespace Defra.Livestock.Sdk.Api.Strategies.Abstractions.Responses;
+
+using Defra.Livestock.Sdk.Api.Strategies.Abstractions.Repositories.Pagination;
+
+public static class PagedEntitiesExtensions
+{
+    public static PagedResults<T> ToPagedResults<T, TEntity>(
+        this PagedEntities<TEntity> pagedEntities,
+        Func<TEntity, T> mapper)
+        => new(
+            pagedEntities.Items.Select(mapper),
+            pagedEntities.TotalCount,
+            pagedEntities.TotalPages,
+            pagedEntities.PageNumber,
+            pagedEntities.PageSize);
+}
